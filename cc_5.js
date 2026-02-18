6a63c3438de8453a4c59fdba1f74731d328d6098
David Albornoz <albornozd@usf.edu>
2026-02-18 18:29:04 -0500

    created array of 3 employee objects

---
+++ cc_5.js
@@ -0,0 +1,5 @@
+let employees = [
+    {name: "John", hourlyRate: 30, hoursWorked: 37},
+    {name: "Stacy", hourlyRate: 25, hoursWorked: 44},
+    {name: "Katherine", hourlyRate: 35, hoursWorked: 51},
+]

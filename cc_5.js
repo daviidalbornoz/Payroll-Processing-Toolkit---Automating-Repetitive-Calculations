71fbb3b89cf77ba33bbcf5fe00057874e21af156
David Albornoz <albornozd@usf.edu>
2026-02-18 19:43:55 -0500

    wrote function to calculate overtime pay that returns 1.5x rate for hours over 40

--- cc_5.js
+++ cc_5.js
@@ -8,3 +8,14 @@ function calculateBasePay(rate, hours) {
     let regularHours = Math.min(hours, 40)
     return regularHours * rate
 }
+
+function calculateOvertimePay(rate, hours) {
+    if (hours > 40) {
+        let overtimeHours = hours - 40
+        return overtimeHours * rate * 1.5
+    }
+    else {
+        return 0
+    }
+}
+

e0be80c125c3cbf2e4e8fb96efdfa712bc697ad7
David Albornoz <albornozd@usf.edu>
2026-02-18 19:37:15 -0500

    wrote a function that calculates base pay up to 40 hours

--- cc_5.js
+++ cc_5.js
@@ -1,5 +1,10 @@
 let employees = [
     {name: "John", hourlyRate: 30, hoursWorked: 37},
     {name: "Stacy", hourlyRate: 25, hoursWorked: 44},
-    {name: "Katherine", hourlyRate: 35, hoursWorked: 51},
+    {name: "Katherine", hourlyRate: 35, hoursWorked: 51}
 ]
+
+function calculateBasePay(rate, hours) {
+    let regularHours = Math.min(hours, 40)
+    return regularHours * rate
+}
